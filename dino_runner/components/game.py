abbaff495b8ace60a830182d6980998a2116be1c
stevensm19 <osmunoz4@misena.edu.co>
2023-03-30 22:36:34 -0500

    Primera clase

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,6 +1,6 @@
 import pygame
-
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
+from dino_runner.components.dinosaur import Dinosour
 
 
 class Game:
@@ -14,6 +14,7 @@ class Game:
         self.game_speed = 20
         self.x_pos_bg = 0
         self.y_pos_bg = 380
+        self.player = Dinosour()
 
     def run(self):
         # Game loop: events - update - draw
@@ -36,8 +37,10 @@ class Game:
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
+        self.player.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
+        self.draw()
 
     def draw_background(self):
         image_width = BG.get_width()
